c1be87b1d6bc0cb704efab76799f22a610403175
rohinvinod <63165746+rohinvinod@users.noreply.github.com>
2020-10-19 12:18:49 -0600

    updated

--- api.js
+++ api.js
@@ -1,12 +1,13 @@
 var city = document.querySelector(".name");
 var temp = document.querySelector(".temp");
 var desc = document.querySelector(".desc");
-var celsius = document.querySelector(".celsius");
-var fahren = document.querySelector(".fahren");
 var button = document.querySelector(".search");
 var cityname = document.querySelector(".cityname");
+var celsius = document.querySelector(".celsius");
+var fahren = document.querySelector(".fahren");
 var d = new Date();
 var n = d.toLocaleTimeString();
+var testing;
 
 button.addEventListener("click", function () {
   fetch(
